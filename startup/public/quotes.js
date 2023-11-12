edc03e3626c3f9fea15f15bf5304bb1e2d9d61ce
Jonathan Ballard <jonballard6@outlook.com>
2023-11-11 22:42:37 -0700

    Fixing bugs in display, adding third-party API call

--- startup/public/quotes.js
+++ startup/public/quotes.js
@@ -1,5 +1,5 @@
 function displayQuote(data) {
-  fetch('https://api.quotable.io/random')
+  fetch('https://api.quotable.io/quotes?author=bruce-lee')
     .then((response) => response.json())
     .then((data) => {
       const containerEl = document.querySelector('#quote');
@@ -9,8 +9,13 @@ function displayQuote(data) {
       const authorEl = document.createElement('p');
       authorEl.classList.add('author');
 
-      quoteEl.textContent = data.content;
-      authorEl.textContent = data.author;
+      // Get a random index from 0 to data.count - 1
+      const randomIndex = Math.floor(Math.random() * data.count);
+      // Get the quote object at that index
+      const quote = data.results[randomIndex];
+
+      quoteEl.textContent = quote.content;
+      authorEl.textContent = quote.author;
 
       containerEl.appendChild(quoteEl);
       containerEl.appendChild(authorEl);
