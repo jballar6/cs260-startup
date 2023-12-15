e66db947df82d6022a9a4a2b14df2d3850373659
Jonathan Ballard <jonballard6@outlook.com>
2023-12-14 22:38:20 -0700

    Scaling back some features to try and get basics working:

--- startup/src/app.jsx
+++ startup/src/app.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css'
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Liftlog } from './liftlog/liftlog';
+import { AuthState } from './login/authState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
 
 function App() {
-    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
-    const isAuthenticated = Boolean(userName);
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
 
-    const logout = () => {
-        setUserName('');
-        localStorage.removeItem('userName');
-    };
+  return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+                <h1>The Lift Log</h1>
+          </nav>
+        </header>
 
-    const login = (username) => {
-        setUserName(username);
-        localStorage.setItem('userName', username);
-    };
+        <Login />
 
-    return (
-        <BrowserRouter>
-            <div className='body bg-dark text-light'>
-                <Routes>
-                    <Route path="/liftlog" element={isAuthenticated ? <Liftlog onLogout={logout} /> : <Login onLogin={login} />} />
-                    <Route path="/" element={<Login onLogin={login} />} />
-                </Routes>
+        <footer className="bg-secondary text-white-50">
+            <div className="container-fluid">
+                <span className="text-reset">
+                    Author Name: Jon B
+                    <a className="text-reset" href="https://github.com/jballar5/cs260-startup">GitHub</a>
+                </span>
             </div>
-            <footer className="bg-secondary text-white-50">
-                <div className="container-fluid">
-                    <span className="text-reset">
-                        Author Name: Jon B
-                        <a className="text-reset" href="https://github.com/jballar5/cs260-startup">GitHub</a>
-                    </span>
-                </div>
-            </footer>
-        </BrowserRouter>
-    
-    );
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
 
 export default App;
--- startup/src/login/login.jsx
+++ startup/src/login/login.jsx
@@ -1,107 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import Button from 'react-bootstrap/Button';
-import {MessageDialog} from './messageDialog';
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
 
-export function Login({ onLogin }) {
-    const [userName, setUserName] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [displayError, setDisplayError] = React.useState(null);
-
-    async function loginUser() {
-        loginOrCreate(`/api/auth/login`);
-    }
-
-    async function createUser() {
-        loginOrCreate(`/api/auth/create`);
-    }
-
-    async function loginOrCreate(endpoint) {
-        const response = await fetch(endpoint, {
-        method: 'post',
-        body: JSON.stringify({email: userName, password: password}),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-        });
-        if (response?.status === 200) {
-            localStorage.setItem('userName', userName);
-            onLogin(userName); // Call the onLogin function passed in as a prop
-        } else {
-            const body = await response.json();
-            setDisplayError(`⚠ Error: ${body.msg}`);
-        }
-    }
-
+export function Login({ userName, authState, onAuthChange }) {
     function displayQuote(data) {
         fetch('https://api.quotable.io/quotes?author=bruce-lee')
             .then((response) => response.json())
             .then((data) => {
-            const containerEl = document.querySelector('#quote');
+                const containerEl = document.querySelector('#quote');
 
-            const quoteEl = document.createElement('p');
-            quoteEl.classList.add('quote');
-            const authorEl = document.createElement('p');
-            authorEl.classList.add('author');
+                const quoteEl = document.createElement('p');
+                quoteEl.classList.add('quote');
+                const authorEl = document.createElement('p');
+                authorEl.classList.add('author');
 
-            // Get a random index from 0 to data.count - 1
-            const randomIndex = Math.floor(Math.random() * data.count);
-            // Get the quote object at that index
-            const quote = data.results[randomIndex];
+                // Get a random index from 0 to data.count - 1
+                const randomIndex = Math.floor(Math.random() * data.count);
+                // Get the quote object at that index
+                const quote = data.results[randomIndex];
 
-            quoteEl.textContent = quote.content;
-            authorEl.textContent = quote.author;
+                quoteEl.textContent = quote.content;
+                authorEl.textContent = quote.author;
 
-            containerEl.appendChild(quoteEl);
-            containerEl.appendChild(authorEl);
-        });
+                containerEl.appendChild(quoteEl);
+                containerEl.appendChild(authorEl);
+            });
     }
 
-    return (
-    <>
-    <header className='container-fluid'>
-        <nav className="navbar fixed-top navbar-dark d-flex flex-column align-items-center justify-content center">
-            <h5>Welcome to</h5>
-            <h1>The Lift Log</h1>
-        </nav>
-    </header>
-    <main className='container-fluid text-center'>
-        <h6 id="quote" style={{ marginTop: '5vh' }}></h6>
-        <div>
-            <form>
-            <div className='input-group mb-3'>
-            <span className='input-group-text'>@</span>
-            <input
-                className='form-control'
-                type='text'
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                placeholder='your@email.com'
-            />
-            </div>
-            </form>
-            <form>
-            <div className='input-group mb-3'>
-            <span className='input-group-text'>🔒</span>
-            <input
-                className='form-control'
-                type='password'
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder='password'
-            />
-            </div>
-            </form>
-            <Button variant='danger' onClick={() => loginUser()}>
-            Login
-            </Button>
-            <Button variant='danger' onClick={() => createUser()}>
-            Create
-            </Button>
-        </div>
-        <img src="assets/BlackRedBodybuilder.png" width="270" />
+    useEffect(() => {
+        displayQuote();
+    }, []);
 
-        <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
-    </main>
-    </>
+    return (
+        <main className='container-fluid text-center'>
+            <div>
+                {authState !== AuthState.Unknown && (
+                    <div>
+                        <h5>Welcome to</h5>
+                        <h1>The Lift Log</h1>
+                    </div>
+                )}
+                {authState === AuthState.Authenticated && (
+                    <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+                )}
+                {authState === AuthState.Unauthenticated && (
+                    <Unauthenticated
+                        userName={userName}
+                        onLogin={(loginUserName) => {
+                            onAuthChange(loginUserName, AuthState.Authenticated);
+                        }}
+                    />
+                )}
+            </div>
+            <h6 id="quote" style={{ marginTop: '5vh' }}></h6>
+            <img src="assets/BlackRedBodybuilder.png" width="270" />
+        </main>
     );
 }
